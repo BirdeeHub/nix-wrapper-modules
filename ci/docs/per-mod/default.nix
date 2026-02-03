59c395e6fcf8f2f90cf0b2c0ec68212d28a92aa6
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-02-03 00:17:29 -0800

    ci(docgen): more progress towards release of docgen api (#276)

--- ci/docs/per-mod/default.nix
+++ ci/docs/per-mod/default.nix
@@ -1,6 +1,82 @@
 { lib, wlib }:
+# TODO: Add doc comments for each function (and generate docs from it once it is in wlib.docs)
 rec {
   collectOptions = import ./collectOptions.nix lib;
-  normWrapperDocs = import ./normopts.nix { inherit lib collectOptions; };
-  wrapperModuleMD = import ./rendermd.nix { inherit wlib lib normWrapperDocs; };
+
+  # TODO: This might not be robust enough?
+  # Maybe check for pairings with all the regular suboptions of _module
+  # Also, should it automatically add relatedPackages to some options somehow?
+  # Technically users can do that themselves with option merging within the modules,
+  # but should we try here? Could be nice?
+  defaultOptionTransform = x: if builtins.elem "_module" x.loc then [ ] else [ x ];
+
+  normWrapperDocs = import ./normopts.nix {
+    inherit
+      wlib
+      lib
+      collectOptions
+      defaultOptionTransform
+      ;
+  };
+
+  fixupDocValues =
+    processTypedText: v:
+    if v ? _type && v ? text then
+      if lib.isFunction processTypedText then
+        processTypedText v
+      else if v._type == "literalExpression" then
+        "```nix\n${toString v.text}\n```"
+      else
+        toString v.text
+    else if lib.isStringLike v && !builtins.isString v then
+      "`<${if v ? name then "derivation ${v.name}" else v}>`"
+    else if builtins.isString v then
+      v
+    else if builtins.isList v then
+      map (fixupDocValues processTypedText) v
+    else if lib.isFunction v then
+      "`<function with arguments ${
+        lib.pipe v [
+          lib.functionArgs
+          (lib.mapAttrsToList (n: v: "${n}${lib.optionalString v "?"}"))
+          (builtins.concatStringsSep ", ")
+        ]
+      }>`"
+    else if builtins.isAttrs v then
+      builtins.mapAttrs (n: fixupDocValues processTypedText) v
+    else
+      v;
+
+  wrapperModuleJSON =
+    {
+      options,
+      graph,
+      transform ? null,
+      includeCore ? true,
+      ...
+    }:
+    lib.pipe
+      {
+        inherit
+          options
+          graph
+          includeCore
+          transform
+          ;
+      }
+      [
+        normWrapperDocs
+        (fixupDocValues null)
+        builtins.toJSON
+        builtins.unsafeDiscardStringContext
+      ];
+
+  wrapperModuleMD = import ./rendermd.nix {
+    inherit
+      wlib
+      lib
+      normWrapperDocs
+      fixupDocValues
+      ;
+  };
 }
