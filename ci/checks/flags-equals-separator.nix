57fa6ef878d3307f4061c0d6d70f5c31c9c588ff
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-02-06 01:35:00 -0800

    ci(modules.makeWrapper): add sanity tests for other wrapper backends.
    Will need more, but this will help for now.

--- ci/checks/flags-equals-separator.nix
+++ ci/checks/flags-equals-separator.nix
@@ -9,9 +9,18 @@ let
     package = pkgs.hello;
     flags = {
       "--greeting" = "hi";
-      "--verbose" = true;
+      "--verbose" = pkgs.lib.mkDefault true;
     };
     flagSeparator = "=";
+  };
+
+  # sanity check binary and shell versions
+  binaryImpl = wrappedPackage.wrap {
+    wrapperImplementation = "binary";
+    flags."--verbose".data = false;
+  };
+  shellImpl = binaryImpl.wrap {
+    wrapperImplementation = pkgs.lib.mkForce "shell";
   };
 
 in
@@ -21,6 +30,15 @@ pkgs.runCommand "flags-equals-separator-test" { } ''
   wrapperScript="${wrappedPackage}/bin/hello"
   if [ ! -f "$wrapperScript" ]; then
     echo "FAIL: Wrapper script not found"
+    exit 1
+  fi
+
+  if ! ${binaryImpl}/bin/hello | grep -q -- "hi"; then
+    echo "FAIL: the pkgs.makeBinaryWrapper implementation does not greet us!"
+    exit 1
+  fi
+  if ! ${shellImpl}/bin/hello | grep -q -- "hi"; then
+    echo "FAIL: the pkgs.makeWrapper implementation does not greet us!"
     exit 1
   fi
 
