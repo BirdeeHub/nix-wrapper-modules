9996c68a6329adbc05fbfe559a2e65db85bfd598
Rachit Kumar Verma <155641117+rachitvrma@users.noreply.github.com>
2026-02-28 17:43:40 +0530

    type(wrapperModules.yt-dlp): init (#312)

---
+++ wrapperModules/y/yt-dlp/check.nix
@@ -0,0 +1,19 @@
+{ pkgs, self }:
+let
+  ytWrapped = self.wrappers.yt-dlp.wrap {
+    inherit pkgs;
+    settings.format = "worst";
+  };
+in
+pkgs.runCommand "yt-dlp-test" { } ''
+  mkdir -p $out
+
+  # Check binary exists
+  test -x "${ytWrapped}/bin/yt-dlp"
+
+  # Check version works
+  "${ytWrapped}/bin/yt-dlp" --version | grep "${ytWrapped.version}"
+
+  # Check help works
+  "${ytWrapped}/bin/yt-dlp" --help > $out/help
+''
---
+++ wrapperModules/y/yt-dlp/module.nix
@@ -0,0 +1,75 @@
+{
+  wlib,
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkOption types;
+  configAtom =
+    with types;
+    oneOf [
+      bool
+      int
+      wlib.types.stringable
+    ];
+
+  renderSingleOption =
+    name: value:
+    let
+      isShort = builtins.stringLength name == 1;
+      prefix = if isShort then "-" else "--";
+    in
+    if lib.isBool value then
+      if value then
+        "${prefix}${name}"
+      else if isShort then
+        ""
+      else
+        "--no-${name}"
+    else
+      "${prefix}${name} ${toString value}";
+
+  renderSettings =
+    settings:
+    lib.pipe settings [
+      (lib.mapAttrsToList (
+        name: value:
+        if lib.isList value then
+          (map (renderSingleOption name) value)
+        else
+          [ (renderSingleOption name value) ]
+      ))
+      builtins.concatLists
+      (lib.remove "")
+      (lib.concatStringsSep "\n")
+    ];
+in
+{
+  imports = [ wlib.modules.default ];
+  options = {
+    settings = mkOption {
+      type = with types; attrsOf (either configAtom (listOf configAtom));
+      default = { };
+      description = "Settings to wrap with the yt-dlp package";
+    };
+  };
+
+  config = {
+    package = pkgs.yt-dlp;
+    flags = {
+      "--config-location" = "${placeholder "out"}/${config.binName}-settings.conf";
+    };
+    drv = {
+      renderedSettings = renderSettings config.settings;
+      passAsFile = [ "renderedSettings" ];
+      buildPhase = ''
+        runHook preBuild
+        cp $renderedSettingsPath "$out/${config.binName}-settings.conf"
+        runHook postBuild
+      '';
+    };
+    meta.maintainers = [ wlib.maintainers.rachitvrma ];
+  };
+}
