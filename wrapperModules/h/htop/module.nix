6cae08dc92e579ba1fe78241876597c8cf83ef32
Alex Lov <alex@alexlov.com>
2026-03-09 18:10:32 +0300

    feat(wrapperModules.htop): init

---
+++ wrapperModules/h/htop/module.nix
@@ -0,0 +1,63 @@
+{
+  config,
+  wlib,
+  lib,
+  pkgs,
+  ...
+}:
+let
+    mkValueString =
+      value:
+      if builtins.isList value then
+        builtins.concatStringsSep " " (map mkValueString value)
+      else if builtins.isString value then
+        value
+      else if builtins.isBool value then
+        if value then "1" else "0"
+      else if builtins.isInt value then
+        toString value
+      else
+        throw "Unrecognized type ${builtins.typeOf value} in htop settings";
+
+    mkKeyValue = lib.generators.mkKeyValueDefault { inherit mkValueString; } "=";
+    toHtopConf = lib.generators.toKeyValue { inherit mkKeyValue; };
+
+    htopConfig = pkgs.writeText "htoprc" (lib.concatLines [
+       # header_layout must be the first in file (or at least just above) so column_meter* parameters can work
+      (toHtopConf (lib.filterAttrs (n: _: n == "header_layout") config.settings))
+      (toHtopConf (lib.filterAttrs (n: _: n != "header_layout") config.settings))
+    ]);
+in
+{
+  imports = [ wlib.modules.default ];
+
+  options = {
+    settings = lib.mkOption {
+      type =
+        with lib.types;
+        attrsOf (oneOf [
+          str
+          int
+          bool
+          (listOf (oneOf [
+            str
+            int
+            bool
+          ]))
+        ]);
+      default = { };
+      example = {
+        hide_kernel_threads = true;
+        hide_userland_threads = true;
+      };
+      description = ''
+        Options to add to HTOPRC
+      '';
+    };
+  };
+
+  config.package = lib.mkDefault pkgs.htop;
+  config.envDefault.HTOPRC = htopConfig;
+
+  meta.maintainers = [ wlib.maintainers.alexlov ];
+}
