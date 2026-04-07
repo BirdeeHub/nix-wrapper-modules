42c5cd6e2bce53cebc3db1dbe0c3b7fb93f2376f
clay53 <clayton@claytondoesthings.xyz>
2026-04-07 00:03:21 -0400

    maybe

---
+++ wrapperModules/e/eww/module.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  lib,
+  wlib,
+  pkgs,
+  ...
+}:
+{
+  imports = [ wlib.modules.default ];
+  options = {
+    yuck = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+    };
+    style = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+    };
+    styleFormat = lib.mkOption {
+      type = lib.types.enum [
+        "css"
+        "scss"
+      ];
+      default = "scss";
+    };
+  };
+
+  config = {
+    package = lib.mkDefault pkgs.eww;
+
+    constructFiles.yuck = {
+      content = config.yuck;
+      relPath = "eww.yuck";
+    };
+
+    constructFiles.style = {
+      content = config.style;
+      relPath = "eww.${config.styleFormat}";
+    };
+
+    config."--config" = config.constructFiles.yuck.outPath;
+    
+    meta.maintainers = [ wlib.maintainers.clay53 ];
+  };
+}
