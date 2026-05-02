f30bb23022b1a192c037edd6d053504194adf7e2
Pancake Appletree <189892522+appleptree@users.noreply.github.com>
2026-05-02 22:56:15 +0200

    feat(wrapperModules.atuin): init

---
+++ wrapperModules/a/atuin/module.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  wlib,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  tomlFmt = pkgs.formats.toml { };
+in
+{
+  imports = [ wlib.modules.default ];
+
+  options = {
+    settings = lib.mkOption {
+      type = tomlFmt.type;
+      default = { };
+      description = ''
+        Atuin configuration options.
+      '';
+    };
+  };
+
+  config = {
+    package = lib.mkDefault pkgs.atuin;
+    env.ATUIN_CONFIG_DIR = "${placeholder "out"}/${config.binName}-config";
+    constructFiles.generatedConfig = {
+      content = builtins.toJSON config.settings;
+      relPath = "${config.binName}-config/config.toml";
+      builder = ''mkdir -p "$(dirname "$2")" && ${pkgs.remarshal}/bin/json2toml "$1" "$2"'';
+    };
+    meta = {
+      maintainers = [ wlib.maintainers.appleptree ];
+      platforms = lib.platforms.linux;
+    };
+  };
+}
