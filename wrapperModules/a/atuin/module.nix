6d350bc6f8a4f1c000b2a6a30ebef9a2ed84be30
Pancake Appletree <189892522+appleptree@users.noreply.github.com>
2026-05-03 00:41:42 +0200

    feat(wrapperModules.atuin): Add support for the atuin-server binary

--- wrapperModules/a/atuin/module.nix
+++ wrapperModules/a/atuin/module.nix
@@ -19,16 +19,31 @@ in
         Atuin configuration options.
       '';
     };
+    server-settings = lib.mkOption {
+      type = tomlFmt.type;
+      default = { };
+      description = ''
+        Atuin server configuration options.
+      '';
+    };
   };
 
   config = {
     package = lib.mkDefault pkgs.atuin;
     env.ATUIN_CONFIG_DIR = "${placeholder "out"}/${config.binName}-config";
-    constructFiles.generatedConfig = {
-      content = builtins.toJSON config.settings;
-      relPath = "${config.binName}-config/config.toml";
-      builder = ''mkdir -p "$(dirname "$2")" && ${pkgs.remarshal}/bin/json2toml "$1" "$2"'';
+    constructFiles = {
+      generatedConfig = {
+        content = builtins.toJSON config.settings;
+        relPath = "${config.binName}-config/config.toml";
+        builder = ''mkdir -p "$(dirname "$2")" && ${pkgs.remarshal}/bin/json2toml "$1" "$2"'';
+      };
+      generatedServerConfig = {
+        content = builtins.toJSON config.server-settings;
+        relPath = "${config.binName}-config/server.toml";
+        builder = ''mkdir -p "$(dirname "$2")" && ${pkgs.remarshal}/bin/json2toml "$1" "$2"'';
+      };
     };
+    wrapperVariants.atuin-server = { };
     meta.maintainers = [ wlib.maintainers.appleptree ];
   };
 }
