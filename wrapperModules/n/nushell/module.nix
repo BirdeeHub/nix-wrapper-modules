aec71655865e753566bcdea012f7c9bca053e34d
Noah765 <noah.landgraf@gmx.de>
2026-06-07 11:42:54 +0200

    feat(wrapperModules.nushell): add login.nu option

--- wrapperModules/n/nushell/module.nix
+++ wrapperModules/n/nushell/module.nix
@@ -39,9 +39,24 @@
         the history file to the Nix store.
       '';
     };
+    "login.nu" = lib.mkOption {
+      type = wlib.types.file {
+        path = lib.mkOptionDefault config.constructFiles.generatedConfig.path;
+      };
+      default = { };
+      description = ''
+        The Nushell login configuration file.
+
+        Provide either `.content` to inline the file contents or `.path` to reference an existing file.
+
+        The configuration directory of Nushell is set to a directory containing this file via
+        the XDG_CONFIG_HOME environment variable.
+      '';
+    };
   };
 
   config.env.XDG_CONFIG_HOME = "${placeholder config.outputName}/${config.binName}-config";
+
   config.constructFiles.generatedConfig = {
     content = config."config.nu".content;
     relPath = "${config.binName}-config/nushell/config.nu";
@@ -50,6 +65,11 @@
     content = config."env.nu".content;
     relPath = "${config.binName}-config/nushell/env.nu";
   };
+  config.constructFiles.generatedLogin = {
+    content = config."login.nu".content;
+    relPath = "${config.binName}-config/nushell/login.nu";
+  };
+
   config.passthru.shellPath = "/bin/nu";
 
   config.wrapperImplementation = "binary";
